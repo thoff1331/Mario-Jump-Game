b030fedf502e0a2f02cddb4bbb7260b35f8a35f8
thoff1331 <trevorhoffman@Trevors-MacBook-Air.local>
2020-12-31 11:53:05 -0700

    finished game

---
+++ script.js
@@ -0,0 +1,29 @@
+let mario = document.getElementById("mario");
+let block = document.getElementById("block");
+let counter = 0;
+function jump() {
+  if (mario.classList == "animate") {
+    return;
+  }
+  mario.classList.add("animate");
+  setTimeout(function () {
+    mario.classList.remove("animate");
+  }, 300);
+}
+let checkDead = setInterval(function () {
+  let characterTop = parseInt(
+    window.getComputedStyle(mario).getPropertyValue("top")
+  );
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+    block.style.animation = "none";
+    alert("Game Over. score: " + Math.floor(counter / 100));
+    counter = 0;
+    block.style.animation = "block 1s infinite linear";
+  } else {
+    counter++;
+    document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+  }
+}, 10);
